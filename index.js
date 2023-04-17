33e8ca3ed9953e19c410a7d7caa97d1ae8474687
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-04-17 16:19:44 -0700

    exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('json-content', (request) => {
+    if (request.method === 'POST') {
+        return JSON.stringify(request.body)
+    }
+} )
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-content'))
 
 let persons = [
     { 
