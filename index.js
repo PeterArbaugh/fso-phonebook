2f4c78a46b6a3ab2c88d3377f056086a44b6bdda
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-04-20 17:18:55 -0700

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('json-content', (request) => {
     if (request.method === 'POST') {
