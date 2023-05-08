0792c4ade9708d8459f7db27e89fa096c8599f30
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-05-08 16:39:53 -0700

    exercise 3.22

--- index.js
+++ index.js
@@ -10,126 +10,128 @@ app.use(cors())
 app.use(express.static('build'))
 
 morgan.token('json-content', (request) => {
-    if (request.method === 'POST') {
-        return JSON.stringify(request.body)
-    }
+	if (request.method === 'POST') {
+		return JSON.stringify(request.body)
+	}
 } )
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-content'))
 
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    console.log('error handler')
-    if (error.name === 'ValidationError') {
-        console.log(`validation error`)
-        // return response.status(400).json({ error: error.message })
-        response.status(400)
-        response.send(error.message)
-    } else {
-        response.status(error.status || 500).json({error: "Internal server error"})
-    }
+	console.error(error.message)
+	console.log('error handler')
+	if (error.name === 'ValidationError') {
+		console.log('validation error')
+		// return response.status(400).json({ error: error.message })
+		response.status(400)
+		response.send(error.message)
+	} else {
+		response.status(error.status || 500).json({ error: 'Internal server error' })
+	}
 }
 
 app.get('/', (request, response) => {
-    response.send('<h1>hello world</h1>')
+	response.send('<h1>hello world</h1>')
 })
 
 app.get('/info', (request, response) => {
-    Person.find({})
-        .then(persons => {
-            const personsCount = persons.length
-            const date = new Date()
-            response.send(`Phonebook has info for ${personsCount} people. <br/><br/> ${date.toString()}`)
-        })
+	Person.find({})
+		.then(persons => {
+			const personsCount = persons.length
+			const date = new Date()
+			response.send(`Phonebook has info for ${personsCount} people. <br/><br/> ${date.toString()}`)
+		})
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
-    .catch(error => next(error))
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    console.log(request.params.id)
-    const person = Person.find({ _id: request.params.id }).then(person => {
-        if (person) {
-            response.json(person)
-            console.log(person)
-        } else {
-            response.status(404)
-            response.json({ error: 'Person not found'})
-        }
-    })
-    .catch(error => next(error))
+	console.log(request.params.id)
+	// eslint-disable-next-line no-unused-vars
+	const person = Person.find({ _id: request.params.id }).then(person => {
+		if (person) {
+			response.json(person)
+			console.log(person)
+		} else {
+			response.status(404)
+			response.json({ error: 'Person not found' })
+		}
+	})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const newPerson = request.body
-    console.log(newPerson)
+	const newPerson = request.body
+	console.log(newPerson)
 
-    // const nameExists = persons.some(person => person.name.toLowerCase() === newPerson.name.toLowerCase())
-    const nameExists = false
+	// const nameExists = persons.some(person => person.name.toLowerCase() === newPerson.name.toLowerCase())
+	const nameExists = false
 
-    if (!newPerson.name || !newPerson.number) {
-        response.status(503)
-        response.json(
-            {error: 'name and number must exist'}
-        )
-    } else if (nameExists) {
-        response.status(503)
-        response.json(
-            {error: 'name already exists in phonebook'}
-        )
-    } else {
-        const person = new Person({
-            name: newPerson.name,
-            number: newPerson.number
-        })
+	if (!newPerson.name || !newPerson.number) {
+		response.status(503)
+		response.json(
+			{ error: 'name and number must exist' }
+		)
+	} else if (nameExists) {
+		response.status(503)
+		response.json(
+			{ error: 'name already exists in phonebook' }
+		)
+	} else {
+		const person = new Person({
+			name: newPerson.name,
+			number: newPerson.number
+		})
 
-        person.save()
-            .then(savedPerson => {
-                response.json(savedPerson)
-            })
-            .catch(error => {
-                console.log(`error caught in put`, error);
-                next(error)
-                //response.status(500).end()
-            })
-    }
+		person.save()
+			.then(savedPerson => {
+				response.json(savedPerson)
+			})
+			.catch(error => {
+				console.log('error caught in put', error)
+				next(error)
+				//response.status(500).end()
+			})
+	}
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    console.log(request.params.id)
-    const updatePerson = request.body
-    console.log(updatePerson)
-    Person.findById(request.params.id )
-        .then(personToUpdate => {
-            personToUpdate.number = updatePerson.number
-            personToUpdate.save()
-                .then(updatedPerson => response.json(updatedPerson))
-                .catch(error => {
-                    console.log(error)
-                    next(error)
-                })
-        .catch(error => {
-            console.log(error)
-            next(error)
-        })
-    })
+	console.log(request.params.id)
+	const updatePerson = request.body
+	console.log(updatePerson)
+	Person.findById(request.params.id )
+		.then(personToUpdate => {
+			personToUpdate.number = updatePerson.number
+			personToUpdate.save()
+				.then(updatedPerson => response.json(updatedPerson))
+				.catch(error => {
+					console.log(error)
+					next(error)
+				})
+				.catch(error => {
+					console.log(error)
+					next(error)
+				})
+		})
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+	Person.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
 
-app.use(errorHandler) 
+app.use(errorHandler)
